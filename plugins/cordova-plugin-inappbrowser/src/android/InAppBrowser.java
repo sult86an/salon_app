92239e7d9e860460e3fb8fd7b4a076bf784e3c29
sult86an <sltn986@gmail.com>
2019-06-15 20:12:15 +0300

    test

--- plugins/cordova-plugin-inappbrowser/src/android/InAppBrowser.java
+++ plugins/cordova-plugin-inappbrowser/src/android/InAppBrowser.java
@@ -42,6 +42,8 @@ import android.webkit.CookieManager;
 import android.webkit.CookieSyncManager;
 import android.webkit.HttpAuthHandler;
 import android.webkit.ValueCallback;
+import android.app.Activity;
+import android.webkit.GeolocationPermissions;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -803,12 +805,6 @@ public class InAppBrowser extends CordovaPlugin {
                 }
                 settings.setDomStorageEnabled(true);
 
-                webView.setWebChromeClient(new WebChromeClient() {
-                 public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
-                    callback.invoke(origin, true, false);
-                 }
-                });
-
                 if (clearAllCache) {
                     CookieManager.getInstance().removeAllCookie();
                 } else if (clearSessionCache) {
@@ -1115,6 +1111,66 @@ public class InAppBrowser extends CordovaPlugin {
 
             // By default handle 401 like we'd normally do!
             super.onReceivedHttpAuthRequest(view, handler, host, realm);
+        }
+    }
+}
+
+public class GeoWebViewActivity extends Activity {
+
+    /**
+     * WebViewClient subclass loads all hyperlinks in the existing WebView
+     */
+    public class GeoWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            // When user clicks a hyperlink, load in the existing WebView
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+    /**
+     * WebChromeClient subclass handles UI-related calls
+     * Note: think chrome as in decoration, not the Chrome browser
+     */
+    public class GeoWebChromeClient extends WebChromeClient {
+        @Override
+        public void onGeolocationPermissionsShowPrompt(String origin,
+                GeolocationPermissions.Callback callback) {
+            // Always grant permission since the app itself requires location
+            // permission and the user has therefore already granted it
+            callback.invoke(origin, true, false);
+        }
+    }
+
+    WebView mWebView;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        mWebView = (WebView) findViewById(R.id.webView1);
+        // Brower niceties -- pinch / zoom, follow links in place
+        mWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        mWebView.getSettings().setBuiltInZoomControls(true);
+        mWebView.setWebViewClient(new GeoWebViewClient());
+        // Below required for geolocation
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setGeolocationEnabled(true);
+        mWebView.setWebChromeClient(new GeoWebChromeClient());
+        // Load google.com
+        mWebView.loadUrl("http://www.google.com");
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Pop the browser back stack or exit the activity
+        if (mWebView.canGoBack()) {
+            mWebView.goBack();
+        }
+        else {
+            super.onBackPressed();
         }
     }
 }
